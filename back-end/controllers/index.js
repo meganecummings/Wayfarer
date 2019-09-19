ecdd3efd4766d4fecc3afdc1fa88aa15b70940fa
edfranco <edfranco415@gmail.com>
2019-09-18 19:34:55 -0700

    split backend and frontend

--- back-end/controllers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-    auth: require('./authController.js'),
-    users: require('./usersController.js'), 
-    city: require('./cityController.js'),
-    post: require('./postController.js'),
-    comments: require('./commentController.js')
-};
--- back-end/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-    auth: require('./auth.js'),
-    users: require('./users.js'), 
-    cities: require('./cities.js'),
-    posts: require('./posts.js'),
-    comments: require('./comments.js'),
-};
