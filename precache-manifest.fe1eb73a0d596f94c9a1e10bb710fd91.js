b2b5486b3992e3ae9dcafe2b49880b2257ea2ebe
meganecummings <megcummings@gmail.com>
2019-09-30 22:20:02 -0700

    Updates

---
+++ precache-manifest.fe1eb73a0d596f94c9a1e10bb710fd91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe3c94571f624ce5ff481d04f81c019",
+    "url": "/wayfarer/index.html"
+  },
+  {
+    "revision": "f0bb3f59a46011a52f8c",
+    "url": "/wayfarer/static/css/main.4cfca004.chunk.css"
+  },
+  {
+    "revision": "36c005d08f4ba4c86c35",
+    "url": "/wayfarer/static/js/2.f4bf64c5.chunk.js"
+  },
+  {
+    "revision": "f0bb3f59a46011a52f8c",
+    "url": "/wayfarer/static/js/main.198d6e09.chunk.js"
+  },
+  {
+    "revision": "07824e738a8d27be066e",
+    "url": "/wayfarer/static/js/runtime~main.33cbf5ef.js"
+  }
+]);
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'https://wayfarer-backend-megancummings.herokuapp.com/api/v1';
